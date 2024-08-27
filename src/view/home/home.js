5e86ef89bf52905d4f22cf6c943b9cd20122daeb
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-08-27 20:12:17 +0530

    react-hot-toast

--- src/view/home/home.js
+++ src/view/home/home.js
@@ -3,6 +3,7 @@ import "./home.css"
 import Navbar from '../../component/navbar/navbar'
 import imghome from './House searching-rafiki.png'
 import Footer from '../../component/footer/footer'
+import toast, { Toaster } from 'react-hot-toast'
 
 function Home() {
   return (
@@ -10,7 +11,18 @@ function Home() {
      <Navbar/>
         <h1 className='page-title'>Home</h1>
         <img src={imghome} className='header-img'/>
+
+        <button type="button" onClick={()=>{
+          toast.loading("loaded please wait...")
+        }}> loading</button>
+
+        <button type="button" onClick={()=>{
+          toast.dismiss()
+          toast.success("Home is loaded successfully")
+        }}> successful</button>
+        
         <Footer/>
+        <Toaster/>
     </div>
   )
 }
